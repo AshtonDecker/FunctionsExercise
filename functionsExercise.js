63e08635e73323f97f64cc9fc88929cd70f8e1b8
Ashton <ashton.decker@stu.barren.kyschools.us>
2024-03-27 10:36:03 -0500

    Complete

---
+++ functionsExercise.js
@@ -0,0 +1,53 @@
+const fozzieBear = function(){
+    console.log(`Wocka Wocka!`);
+}
+fozzieBear();
+
+const beaker = function(speak){
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+}
+beaker(`Meep`);
+
+const muppetShow = function(a, b){
+    if (a = "Muppet") and (b = "Show");{
+        return `It's time to play the music. It's time to light the lights.`;
+    }
+
+}
+muppetShow("Muppet", "Show")
+
+const Kermit = function(){
+    console.log("Kermit The Frog");
+}
+
+const muppetShowSeasons = function(seasons){
+    if (seasons == 5) ;{
+        return true
+    }
+        return false
+}
+
+const manOrMuppet = function(){
+    console.log("Am I a man or am I a Muppet?");
+}
+
+rainbowConnection = () => (
+    console.log("Someday we'll find it. The Rainbow Connection. The lovers, the dreamers and me")
+)
+
+// Yes
+// No
+
+const newMuppetMovies = 
+    [
+        `The Muppets`,
+        `Muppets Most Wanted`
+    ]
+
+const newArr = to.Uppercase(newMuppetMovies)
+const upperMovies = (newArr) => {
+    return newMuppetMovies;
+}
